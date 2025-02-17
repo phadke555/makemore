7088e753f357e043e49bcc79b93a5099bd6dd91f
Rohan Phadke <rohanphadke@gmail.com>
2025-02-17 22:38:20 +1100

    complete minitorch and mlp/wavenet implementations

--- minitorch.py
+++ minitorch.py
@@ -78,6 +78,16 @@ class Embedding:
 
 # ----------------------------------------------------------------------------
 
+class Flatten:
+    def __call__(self, x):
+        self.out = x.view(x.shape[0], -1)
+        return self.out
+    
+    def parameters(self):
+        return []
+
+# ----------------------------------------------------------------------------
+
 class FlattenConsecutive:
     def __init__(self, n):
         self.n = n
