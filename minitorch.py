8ab08c191871ccd07b70496ddcfffe74550346c3
Rohan Phadke <rohanphadke@gmail.com>
2025-02-17 22:07:25 +1100

    create minitorch and mlp, wavenet implementations

---
+++ minitorch.py
@@ -0,0 +1,109 @@
+import torch
+
+class Linear:
+    def __init__(self, input_dim, output_dim, bias = True):
+        self.weight = torch.randn((input_dim, output_dim)) / (input_dim ** 0.5)  # kaiming init
+        self.bias = torch.zeros(output_dim) if bias else None
+    
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias is not None:
+            self.out = self.out + self.bias
+        return self.out
+    
+    def parameters(self):
+        return [self.weight] + ([] if self.bias is not None else [self.bias])
+
+# ----------------------------------------------------------------------------
+    
+    
+class BatchNorm1d:
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps = eps
+        self.momentum = momentum
+        self.training = True
+        
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+        
+        self.running_mean = torch.zeros(dim)
+        self.running_var = torch.ones(dim)
+        
+    def __call__(self, x):
+        if self.training:
+            if x.ndim == 2:
+                dim = 0
+            elif x.ndim == 3:
+                dim = (0, 1)
+            xmean = x.mean(dim, keepdim = True)
+            xvar = x.var(dim, keepdim = True)
+        
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+            
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps)
+        self.out = self.gamma * xhat + self.beta
+        
+        if self.training:
+            with torch.no_grad():
+                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * xmean
+                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * xvar
+        
+        return self.out
+        
+    def parameters(self):
+        return [self.gamma, self.beta]
+
+# ----------------------------------------------------------------------------
+
+class Tanh:
+    def __call__(self, x):
+        self.out = torch.tanh(x)
+        return self.out
+    def parameters(self):
+        return []
+    
+# ----------------------------------------------------------------------------
+
+class Embedding:
+    def __init__(self, num_embeddings, embedding_dim):
+        self.weight = torch.randn((num_embeddings, embedding_dim))
+    
+    def __call__(self, ix):
+        self.out = self.weight[ix]
+        return self.out
+    def parameters(self):
+        return [self.weight]
+
+# ----------------------------------------------------------------------------
+
+class FlattenConsecutive:
+    def __init__(self, n):
+        self.n = n
+    
+    def __call__(self, x):
+        B, T, C = x.shape
+        x = x.view(B, T // self.n, C * self.n)
+        if x.shape[1] == 1:
+            x = x.squeeze(1)
+        self.out = x
+        return self.out
+    
+    def parameters(self):
+        return []
+    
+# ----------------------------------------------------------------------------
+
+class Sequential:
+    def __init__(self, layers):
+        self.layers = layers
+    
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        self.out = x
+        return self.out
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
