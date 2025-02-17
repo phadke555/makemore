160844fbb5017a63f2965026f090b86048e53982
Rohan Phadke <rohanphadke@gmail.com>
2025-02-18 10:22:07 +1100

    add transformer classes

--- minitorch.py
+++ minitorch.py
@@ -117,3 +117,139 @@ class Sequential:
     
     def parameters(self):
         return [p for layer in self.layers for p in layer.parameters()]
+    
+# ----------------------------------------------------------------------------
+
+class SelfAttentionHead(nn.Module):
+  """ one self attention head """
+  def __init__(self, head_size):
+    super().__init__()
+    self.query = nn.Linear(n_embd, head_size, bias=False)
+    self.key = nn.Linear(n_embd, head_size, bias=False)
+    self.value = nn.Linear(n_embd, head_size, bias=False)
+    self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, x):
+    B, T, C = x.shape
+    k = self.key(x)
+    q = self.query(x)
+
+    # compute attention scores / affinities between words
+    wei = q @ k.transpose(-2, -1) * C**-0.5
+    wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+    wei = F.softmax(wei, dim=-1)
+    wei = self.dropout(wei)
+
+    # weighted aggregation of values
+    v = self.value(x)
+    out = wei @ v
+    return out
+
+class MultiHead(nn.Module):
+  """ multiple self attention heads concatenated """
+  def __init__(self, num_heads, head_size):
+    super().__init__()
+    self.heads = nn.ModuleList([SelfAttentionHead(head_size) for _ in range(num_heads)])
+    self.projection = nn.Linear(n_embd, n_embd)
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, x):
+    out = torch.cat([h(x) for h in self.heads], dim=-1)
+    out = self.projection(out)
+    out = self.dropout(out)
+    return out
+
+class FeedForward(nn.Module):
+  def __init__(self, n_embd):
+    super().__init__()
+    self.net = nn.Sequential(
+      # n_embd is the input size, 4*n_embd is the hidden dimension size
+      nn.Linear(n_embd, 4 * n_embd),
+      nn.ReLU(),
+      nn.Linear(4 * n_embd, n_embd),
+      nn.Dropout(dropout)
+    )
+
+  def forward(self, x):
+    out = self.net(x)
+    return out
+
+class LayerNorm1d(nn.Module):
+  def __init__(self, dim, eps=1e-5):
+    super().__init__()
+    self.eps = eps
+    self.gamma = torch.ones(dim)
+    self.beta = torch.zeros(dim)
+
+  def __call__(self, x):
+    xmean = x.mean(dim=1, keepdim=True)
+    xvar = x.var(dim=1, keepdim=True)
+    xhat = (x - xmean) / torch.sqrt(xvar + self.eps)
+    self.out = self.gamma * xhat + self.beta
+    return self.out
+
+  def parameters(self):
+    return [self.gamma, self.beta]
+
+class Block(nn.Module):
+  def __init__(self, n_embd, n_head):
+    super().__init__()
+    head_size = n_embd // n_head
+    self.a = MultiHead(n_head, head_size)
+    self.ff = FeedForward(n_embd)
+    self.ln1 = nn.LayerNorm(n_embd)
+    self.ln2 = nn.LayerNorm(n_embd)
+
+  def forward(self, x):
+    # use of residual connections
+    x = x + self.a(self.ln1(x))
+    x = x + self.ff(self.ln2(x))
+    return x
+
+class LanguageModel(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+    self.position_embedding_table = nn.Embedding(block_size, n_embd)
+    self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+    self.ln = nn.LayerNorm(n_embd)
+    self.lm_head = nn.Linear(n_embd, vocab_size)
+
+  def forward(self, idx, targets=None):
+    B, T = idx.shape
+
+    tok_emb = self.token_embedding_table(idx)
+    pos_emb = self.position_embedding_table(torch.arange(T, device=device))
+
+    x = tok_emb + pos_emb
+    x = self.blocks(x)
+    x = self.ln(x)
+    logits = self.lm_head(x)
+
+    if targets is None:
+      loss = None
+    else:
+      B, T, C = logits.shape
+      logits = logits.view(B*T, C)
+      targets = targets.view(B*T)
+      loss = F.cross_entropy(logits, targets)
+
+    return logits, loss
+  def generate(self, idx, max_new_tokens):
+    # idx is (B, T) array of indices in the current context
+    for _ in range(max_new_tokens):
+      # crop idx to the last block_size tokens
+      idx_cond = idx[:, -block_size:]
+      # get the predictions
+      logits, loss = self(idx_cond)
+      # focus only on the last time step
+      logits = logits[:, -1, :] # becomes (B, C)
+      # apply softmax to get probabilities
+      probs = F.softmax(logits, dim=-1) # (B, C)
+      # sample from the distribution
+      idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+      # append sampled index to the running sequence
+      idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+    return idx
